35cc9dc4c21441fd5043f1a247731f3e581e8952
XiaoFeng-QWQ <q1432777209@126.com>
2024-04-27 17:41:14 +0800

    修改pjax超时时间

--- public/js/pjax.js
+++ public/js/pjax.js
@@ -2,17 +2,17 @@
  *   ---- 全站Pjax ----
  * ---------------------*/
 $(document).pjax('a:not(a[target="_blank"],a[no-pjax])', {
-  container: 'main',
-  fragment: 'main',
-  timeout: 8000
+    container: 'main',
+    fragment: 'main',
+    timeout: 20000
 });
 
 $(document).on('pjax:send', function () {
-  NProgress.start();
+    NProgress.start();
 });
 
 $(document).on('pjax:end', function () {
-  showList();
-  showDetailed();
-  NProgress.done();
+    showList();
+    showDetailed();
+    NProgress.done();
 });
