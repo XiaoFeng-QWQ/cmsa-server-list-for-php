0aff51396b07b9d2e8e3f539f61a3c5d585a5e2c
XiaoFeng-QWQ <q1432777209@126.com>
2024-05-23 13:47:58 +0800

    回退目录结构

--- api/index.php
+++ api/index.php
@@ -1,18 +1,8 @@
 <?php
-// PHP 版本判断
-if (version_compare(phpversion(), '5.4', '<')) {
-    header('Content-type:text/html;charset=utf-8');
-    echo sprintf(
-        '<h3>程序运行失败：</h3><blockquote>您的 PHP 版本低于最低要求 5.4，当前版本为 %s</blockquote>',
-        phpversion()
-    );
-    exit;
+$file = __DIR__ . '/..' . $_SERVER["PHP_SELF"];
+
+if (file_exists($file)) {
+    return false;
+} else {
+    require_once __DIR__ . '/../index.php';
 }
-
-require __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/Core/Define.php';
-
-use Framework\Mian;
-
-$App = new Mian;
-$App->run();
